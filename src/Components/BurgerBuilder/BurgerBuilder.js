8cbdf951a176f4116cf479774119e8596d2fc6ee
ishanmazhar <ishanmazhar2@gmail.com>
2021-02-06 12:37:09 +0600

    Navigation Bar

--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -3,9 +3,7 @@ import React, { Component }  from 'react';
 export default class BurgerBuilder extends Component {
     render() {
         return (
-            <div>
-                <p>Burger Builder</p>
-            </div>
+           <div>Burger Builder</div>
         );
     }
 }
