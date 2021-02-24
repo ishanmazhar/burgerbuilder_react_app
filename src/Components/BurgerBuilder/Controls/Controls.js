90d954d40395b697feb665637e345053fbd060aa
ishanmazhar <ishanmazhar2@gmail.com>
2021-02-24 10:43:14 +0600

    Add or Remove Ingredients

--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -12,8 +12,8 @@ const BuildControl = props => {
         <div className="d-flex">
             <div className="mr-auto ml-5" style={{
                 fontWeight: "bold", fontSize: "1.2rem"}}>{props.label}</div>
-            <button className="btn btn-danger btn-sm m-1">Less</button>
-            <button className="btn btn-success btn-sm m-1">More</button>
+            <button className="btn btn-danger btn-sm m-1"  onClick={props.removed}>Less</button>
+            <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
         </div>
     )
 }
@@ -37,7 +37,9 @@ const Controls = props => {
                                 <BuildControl 
                                     label={item.label}
                                     type={item.type}
-                                    key={Math.random()} />
+                                    key={Math.random()}
+                                    added={() => props.ingredientAdded(item.type)}
+                                    removed={() => props.ingredientRemoved(item.type)} />
                             );
                         })
                     }
