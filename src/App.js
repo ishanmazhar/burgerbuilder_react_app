cc503699deb8e5303be1c6eb21434c46f56b4800
ishanmazhar <ishanmazhar2@gmail.com>
2021-05-22 00:28:36 +0600

    Adding Redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react'; 
 import './App.css';
 import Main from './Components/Main'; 
+
 import {BrowserRouter} from 'react-router-dom';
+
+import {provider} from 'react-redux';
+import {store} from './redux/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
--- src/Components/BurgerBuilder/Controls/Controls.js
+++ src/Components/BurgerBuilder/Controls/Controls.js
@@ -10,9 +10,8 @@ const controls = [
 const BuildControl = props => {
     return (
         <div className="d-flex">
-            <div className="mr-auto ml-5" style={{
-                fontWeight: "bold", fontSize: "1.2rem"}}>{props.label}</div>
-            <button className="btn btn-danger btn-sm m-1"  onClick={props.removed}>Less</button>
+            <div className="mr-auto ml-5" style={{ fontWeight: "bold", fontSize: "1.2rem" }}>{props.label}</div>
+            <button className="btn btn-danger btn-sm m-1" onClick={props.removed}>Less</button>
             <button className="btn btn-success btn-sm m-1" onClick={props.added}>More</button>
         </div>
     )
