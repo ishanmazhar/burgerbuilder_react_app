06b9d73637793f22cea0e07a1685506cfe9c1170
ishanmazhar <ishanmazhar2@gmail.com>
2021-05-23 02:10:05 +0600

    Completing Orders Component

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchOrders } from '../../redux/actionCreators'; 
 
+import Order from './Order/Order'; 
+import Spinner from '../Spinner/Spinner'; 
+
 const mapStateToProps = state => {
     return {
         orders: state.orders,
@@ -22,9 +25,33 @@ class Orders extends Component {
         console.log(this.props); 
     }
     render() {
+        let orders = null;
+        if(this.props.orderErr) {
+            orders = <p style= {{
+                border: "1px solid grey",
+                boxShadow: "1px 1px #888888",
+                borderRadius: "5px",
+                padding: "20px",
+                marginBottom: "10px", 
+            }}>Sorry! Failed to load orders!</p>
+        } else {
+            if (this.props.orders.length === 0) {
+                orders = <p style= {{
+                    border: "1px solid grey",
+                    boxShadow: "1px 1px #888888",
+                    borderRadius: "5px",
+                    padding: "20px",
+                    marginBottom: "10px", 
+                }}>You have no order!</p> 
+            } else {
+                orders = this.props.orders.map(order => {
+                    return <Order order={order} key={order.id} />
+                })
+            }
+        }
         return (
             <div>
-    
+                {this.props.orderLoading ? <Spinner /> : orders} 
             </div>
         )
     }
